3f98e3e77f446bb0d81b9748b2d315011312d4e7
OchiMoto21 <63431850+OchiMoto21@users.noreply.github.com>
2021-09-03 21:05:11 +0700

    Added looping queue & see queue, Fixed looping

--- commands/play.js
+++ commands/play.js
@@ -4,8 +4,8 @@ const voiceDiscord = require('@discordjs/voice');
 const queue = new Map();
 module.exports = {
     name: 'play',
-    aliases: ['pause', 'stop','loop'],
-    description: 'play a video from youtube',
+    aliases: ['pause', 'stop','loop', 'queue'],
+    description: 'Play a video from youtube',
     
     async execute(message, args, cmd, client, Discord){
         const channel = message.member.voice.channel;
@@ -16,29 +16,53 @@ module.exports = {
         
 
         let server_queue = queue.get(message.guild.id);
-        if (cmd == "loop") {
+        if (cmd === "queue") {
             if (server_queue){
-                server_queue.loop = !server_queue.loop;
+                if (!server_queue.songs.length) return message.channel.send("There's no queue!");
+                return message.channel.send(server_queue.songs.join('\n'));
             } else {
-		message.channel.send("There's no song to loop on!");
-	    }
+	    	    return message.channel.send("There's no queue!");
+	        }
         }
-	if (cmd == "pause") {
+
+        if (cmd === "loop") {
             if (server_queue){
-		pause_player(server_queue);
+                server_queue.loop += 1;
+                if (server_queue.loop === 1) {
+                    message.channel.send("The song is now looped!");
+                }
+                if (server_queue.loop === 2) {
+                    message.channel.send("The queue is now looped!");
+                }
+                if (server_queue.loop === 3) {
+                    server_queue.loop = 0;
+                    message.channel.send("The queue is not looped!");
+                }
             } else {
-	    	message.channel.send("There's no player to be paused!");
-	    }
+		    message.channel.send("There's no song to loop on!");
+	        }
+        }
+
+	    if (cmd === "pause") {
+            if (server_queue){
+                if (server_queue.playing){
+                    server_queue.player.pause();
+                    server_queue.playing = false;
+                    return message.channel.send("The player is paused!");
+                }
+            } else {
+	    	    return message.channel.send("There's no player to be paused!");
+	        }
         } 
         
         if(cmd === "play"){
-	    if (server_queue){
-		 server_queue.player.on(voiceDiscord.AudioPlayerStatus.Paused, () => {
-			server_queue.player.unpause();
-			return message.channel.send("The player is unpaused!");
-		 });
-	    }
-			
+            if (!args.length && server_queue) {
+                if(!server_queue.playing){
+                    server_queue.player.unpause();
+                    server_queue.playing = true;
+                    return message.send.channel("The player is unpaused!");
+                }
+            }
             if(!args.length) return message.channel.send('You need to add link in the second arguments!');
             const videoFinder = async (query) => {
                 const videoResult = await ytSearch(query);
@@ -55,25 +79,54 @@ module.exports = {
                         guildId: message.guild.id,
                         adapterCreator: message.guild.voiceAdapterCreator
                     });
-                    const player = voiceDiscord.createAudioPlayer();
+                    const player = voiceDiscord.createAudioPlayer( {
+                        behaviors:  {
+                            noSubscriber: voiceDiscord.NoSubscriberBehavior.Pause
+                        }
+                    }
+                    );
                     const queue_constructor = {
                         guild: message.guild.id,
                         channel: message.channel, 
                         connection: connection,
                         player: player,
+                        playing: false,
+                        timeout: null,
+                        loop: 0,
                         songs: [],
-                        loop: false
                     }
                     queue_constructor.connection.subscribe(queue_constructor.player);
                     queue.set(message.guild.id, queue_constructor);
-                    console.log("_____________\n queue is made \n_________________");
+                    console.log("-------------\nQueue is made\n-------------");
                     server_queue = queue_constructor;
                     server_queue.songs.push(video.url);
-                    video_player(server_queue);
+                    var n = 0;
+                    video_player(server_queue,message,n);
+                    
+                    server_queue.player.on('error', error => {
+                        server_queue.songs.shift();
+                        server_queue.channel.send('There is an error playing the song!');
+                        n += 1;
+                        video_player(server_queue,message,n);
+                    });
+                    
+                    server_queue.player.on(voiceDiscord.AudioPlayerStatus.Idle, () => {
+                        if (server_queue.loop === 1) {
+                            video_player(server_queue, message, n);
+                        } else {
+                            n += 1;
+                            if ((server_queue.loop === 2) && (server_queue.songs.length === (n))) {
+                                n = 0;
+                                video_player(server_queue, message, n);
+                            } else {
+                                video_player(server_queue, message, n);
+                            }
+                        }
+                    })
                     return message.channel.send(`👍 **${video.title}** added to queue!`);
                 } else if (server_queue.songs.length === 0) {
                     server_queue.songs.push(video.url);
-                    video_player(server_queue);
+                    video_player(server_queue,message,0);
                     return message.channel.send(`👍 **${video.title}** added to queue!`);
                 } else {
                     server_queue.songs.push(video.url);
@@ -85,29 +138,16 @@ module.exports = {
         }
         if(cmd === "stop"){
             if(server_queue){
-		server_queue.player.stop();
-                queue.delete(server_queue.guild);
-		
-            }
-            const connection = voiceDiscord.getVoiceConnection(message.guild.id);
-            if (connection){
-                connection.destroy();
-            }
-        }  
+		        stop_player(server_queue,message);
+            }  
+        }
     }
 }
 
-const pause_player = async (server_queue) => {
-    server_queue.player.on(voiceDiscord.AudioPlayerStatus.Playing, () => {
-	   server_queue.player.pause();
-	   console.log("The player is paused");
-	   return message.channel.send("The player is paused!");
-    });
-}
-
-const video_player = async (server_queue) => {
-    if(server_queue.songs.length > 0){
-        const stream = ytdl(server_queue.songs[0],{
+const video_player = async (server_queue,message,n) => {
+    if(n < server_queue.songs.length){
+        clearTimeout(server_queue.timeout);
+        const stream = ytdl(server_queue.songs[n],{
             filter:'audioonly', 
             opusEncoded: true,
             liveBuffer: 20000,
@@ -117,19 +157,24 @@ const video_player = async (server_queue) => {
         
         try {
             server_queue.player.play(resource);
+            server_queue.playing = true;
         } catch(err) {
             console.log(err);
         }
-        server_queue.player.on('error', error => {
-		server_queue.songs.shift();
-		server_queue.channel.send('There is an error playing the song!');
-		video_player(server_queue);
-	});
-        server_queue.player.on(voiceDiscord.AudioPlayerStatus.Idle, () => {
-            if (!server_queue.loop){
-                server_queue.songs.shift();
-            }
-            video_player(server_queue);
-        })
+    } else {
+        console.log(n);
+        console.log('Timeout set!')
+        server_queue.timeout = setTimeout(() => stop_player(server_queue,message), 5_000);
+    }
+
+}
+
+const stop_player = async (server_queue,message) => {
+    server_queue.player.stop();
+    queue.delete(server_queue.guild);
+
+    const connection = voiceDiscord.getVoiceConnection(message.guild.id);
+    if (connection){
+        connection.destroy();
     }
 }
