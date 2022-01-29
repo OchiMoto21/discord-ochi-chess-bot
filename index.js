6a260b2603d122d4f79c4b7db0ad85b6951cf3be
OchiMoto21 <nikencarolina@gmail.com>
2022-01-29 15:56:03 +0700

    Using Process Environment Token

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+// const config = require('./config.json');
 const client = new Discord.Client({
     intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_VOICE_STATES]
 });
@@ -13,26 +13,4 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-client.login(config.token);
-
-// const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-// for (const file of commandFiles){
-//     const command = require(`./commands/${file}`);
-
-//     client.commands.set(command.name, command);
-// }
-
-// client.once('ready', () =>{
-//     console.log('Chess Bot is ready to go!');
-// });
-
-// client.on('message', message =>{
-//     if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-//     const args = message.content.slice(prefix.length).split(/ +/);
-//     const command = args.shift().toLowerCase();
-
-//     if(command === 'ping'){
-//         client.commands.get('ping').execute(message, args);
-//     } 
-// });
+client.login(process.env.DJS_TOKEN);
