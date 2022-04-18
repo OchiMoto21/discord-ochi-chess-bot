8f7868ec5a743af69195af287a55f3bde5bc26cb
OchiMoto21 <nikencarolina@gmail.com>
2022-04-18 19:10:19 +0700

    Error handling in embed,js and start.js

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -1,11 +1,15 @@
+// https://discordjs.guide/creating-your-bot/command-handling.html#individual-command-files
 module.exports = async (Discord, client, interaction) => {
-    if (!interaction.isCommand()) return;
+	if (!interaction.isCommand()) return;
 
 	const { commandName } = interaction;
 
-	if (commandName === 'react') {
-		const message = await interaction.reply('You can react with Unicode emojis!', { fetchReply: true });
-		message.react('😄');
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	}
 
 }
