be41eeed126dafa1dae580ea9b8e83d4b4284b49
OchiMoto21 <nikencarolina@gmail.com>
2022-04-19 22:26:05 +0700

    Finished Text File

--- commands/embed.js
+++ commands/embed.js
@@ -33,8 +33,8 @@ module.exports = {
                                             var m = String(str[i]).split(',');
                                             console.log(str[i]);
                                             console.log(m.length);
-                                            var title_state = m[1].startsWith("title.");
                                             var image_state = m[0].startsWith("image.no");
+                                            var title_state = m[1].startsWith("title.");
                                             if(title_state){
                                                 if (image_state){
                                                     m.shift();
@@ -46,7 +46,7 @@ module.exports = {
                                                 m.shift();
                                                 console.log(m.length);
                                                 console.log(m);
-                                                if (!m.length == 0 && m.length/2 <= 5){
+                                                if (!m.length == 0 && m.length/2 <= 5 && m.length % 2 == 0){
                                                     for (var j = 1; j <= (m.length)/2; ++j) {
                                                         iWantButtonsDaddy[j] = new Discord.MessageButton()
                                                         .setLabel(m[j*2-2])
@@ -223,35 +223,84 @@ module.exports = {
                 try {
                     if (!args.length) return message.channel.send("There's no argument D:");
                     var iWantButtonsDaddy = [];
-                    // first and second argument is channelid and messageid            
-        
-                    // Alternative syntax using RegExp constructor
-                    // const regex = new RegExp('([^,]+)', 'gm')
-        
                     const m = args.join(" ").split(',');
                     console.log(m);
-                    if(m.length % 2 == 0){
+                    var title = ""; 
+                    var channelID = m[0];
+                    var messageID = m[1];
+                    m.shift();
+                    m.shift();
+                    var title_state = m[0].startsWith("title.");
+                    
+                    if (title_state){
+                        m.shift();
+                    } else {
+                        var title = m[0];
+                        m.shift();
+                    }
+                    
+                    if(!m.length == 0 && m.length/2 <= 5 && m.length % 2 == 0){
                         console.log(m.length);
-                        for (var i = 1; i < m.length/2; ++i) {
-                            iWantButtonsDaddy[i] = new Discord.MessageButton()
-                            .setLabel(m[i*2])
+                        for (var j = 1; j <= (m.length)/2; ++j) {
+                            iWantButtonsDaddy[j] = new Discord.MessageButton()
+                            .setLabel(m[j*2-2])
                             .setStyle('LINK')
-                            .setURL(m[i*2+1])
+                            .setURL(m[j*2-1])
                         }
                         
-                        const row = new Discord.MessageActionRow()
+                        var row = new Discord.MessageActionRow()
                         .addComponents(
                             iWantButtonsDaddy
                         );
-            
                         if (message.attachments.size === 1){
-                            client.channels.cache.get(m[0]).messages.fetch(m[1])
+                            if (title_state){
+                                client.channels.cache.get(channelID).messages.fetch(messageID)
+                                    .then(msg => 
+                                        msg.edit({
+                                            embeds : [memberMessageEmbed
+                                                .setTitle(title)
+                                                .setColor('#dc661f')
+                                                .setImage(message.attachments.first().url)
+                                                ],
+                                            components: [row]
+                                        })
+                                    )
+                                    .catch(
+                                        console.error,
+                                        message.channel.send({embeds : [memberMessageEmbed
+                                            .setTitle("Not a valid argument.")
+                                            .setDescription("False message ID or channel ID.\nThis message will be deleted in 10 seconds.")
+                                            ]}).then(msg => {setTimeout(() => msg.delete(), 10000)})
+                                    );
+                            } else {
+                                client.channels.cache.get(channelID).messages.fetch(messageID)
+                                    .then(msg => 
+                                        msg.edit({
+                                            embeds : [memberMessageEmbed
+                                                .setColor('#dc661f')
+                                                .setImage(message.attachments.first().url)
+                                                ],
+                                            components: [row]
+                                        })
+                                    )
+                                    .catch(
+                                        console.error,
+                                        message.channel.send({embeds : [memberMessageEmbed
+                                            .setTitle("Not a valid argument.")
+                                            .setDescription("False message ID or channel ID.\nThis message will be deleted in 10 seconds.")
+                                            ]}).then(msg => {setTimeout(() => msg.delete(), 10000)})
+                                    );
+                            }
+                                message.delete()
+                        } else {
+                            if (title_state){
+                                client.channels.cache.get(channelID).messages.fetch(messageID)
                                 .then(msg => 
                                     msg.edit({
                                         embeds : [memberMessageEmbed
+                                            .setTitle(title)
                                             .setColor('#dc661f')
-                                            .setImage(message.attachments.first().url)
-                                            ],
+                                        ],
                                         components: [row]
                                     })
                                 )
@@ -262,21 +311,22 @@ module.exports = {
                                         .setDescription("False message ID or channel ID.\nThis message will be deleted in 10 seconds.")
                                         ]}).then(msg => {setTimeout(() => msg.delete(), 10000)})
                                 );
-                                message.delete()
-                        } else {
-                            client.channels.cache.get(m[0]).messages.fetch(m[1])
-                            .then(msg => 
-                                msg.edit({
-                                    components: [row]
-                                })
-                            )
-                            .catch(
-                                console.error,
-                                message.channel.send({embeds : [memberMessageEmbed
-                                    .setTitle("Not a valid argument.")
-                                    .setDescription("False message ID or channel ID.\nThis message will be deleted in 10 seconds.")
-                                    ]}).then(msg => {setTimeout(() => msg.delete(), 10000)})
-                            );
+                            }
+                            else{
+                                client.channels.cache.get(channelID).messages.fetch(messageID)
+                                .then(msg => 
+                                    msg.edit({
+                                        components: [row]
+                                    })
+                                )
+                                .catch(
+                                    console.error,
+                                    message.channel.send({embeds : [memberMessageEmbed
+                                        .setTitle("Not a valid argument.")
+                                        .setDescription("False message ID or channel ID.\nThis message will be deleted in 10 seconds.")
+                                        ]}).then(msg => {setTimeout(() => msg.delete(), 10000)})
+                                );
+                            }
                             message.delete()
                         }
                     } else {
