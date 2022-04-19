2ebf1bf7f3bcaffdad8fb5869c130d3f47786bec
OchiMoto21 <nikencarolina@gmail.com>
2022-04-19 07:30:10 +0700

    brand new command

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-//const config = require('./config.json');
+const config = require('./config.json');
 const client = new Discord.Client({
     intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_VOICE_STATES]
 });
@@ -12,5 +12,5 @@ client.events = new Discord.Collection();
 ['command_handler', 'event_handler'].forEach(handler =>{
     require(`./handlers/${handler}`)(client, Discord);
 })
-//client.login(config.TOKEN);
-client.login(process.env.DJS_TOKEN);
+client.login(config.TOKEN);
+//client.login(process.env.DJS_TOKEN);
