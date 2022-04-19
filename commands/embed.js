84f22cc3f135db2c7a7d4edb0ab913f423c0cc99
OchiMoto21 <nikencarolina@gmail.com>
2022-04-19 13:45:16 +0700

    Text file

--- commands/embed.js
+++ commands/embed.js
@@ -14,7 +14,7 @@ module.exports = {
                     if (!args.length){
                         if(message.attachments.size === 1){
                             console.log("Yes, there's an attachemnt");
-                            request(message.attachments.first().url, 
+                            return request(message.attachments.first().url, 
                                 function (error, response, body) {
                                     console.error('error:', error); // Print the error if one occurred
                                     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
