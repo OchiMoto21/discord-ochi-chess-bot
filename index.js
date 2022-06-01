b4349be38445ec041e487ba4f6658964aab4a4d3
OchiMoto21 <nikencarolina@gmail.com>
2022-06-01 23:03:20 +0700

    KAela Bonk

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+//const config = require('./config.json');
 //require('./deploy_command.js')
 
 //const { MongoClient, ServerApiVersion } = require('mongodb');
