59b17576cd4f7134f3e1199309698c4ca098d5c9
OchiMoto21 <nikencarolina@gmail.com>
2022-06-14 21:29:02 +0700

    command update

---
+++ handlers/createEmbedBuilder.js
@@ -0,0 +1,19 @@
+const EmbedBuilder = require('../schemas/EmbedBuilder')
+const mongoose = require('mongoose')
+module.exports = (client) => {
+    client.createEmbedBuilder = async (guild,member) =>{
+        let EmbedBuilderUser = await EmbedBuilder.findOne({memberId: member.id})
+        if (EmbedBuilderUser){
+            return EmbedBuilderUser;
+        } else {
+            //make a new data if it's not found in the database
+            EmbedBuilderUser = await new EmbedBuilder({
+                _id: mongoose.Types.ObjectId(),
+                guildId: guild.id,
+                memberId: member.id
+            })
+            await EmbedBuilderUser.save().catch(err=> console.log(err));
+            return EmbedBuilderUser;
+        }
+    };
+}
