0c53db609a24408b981737d136295a2418c7fa40
OchiMoto21 <nikencarolina@gmail.com>
2022-07-14 13:37:36 +0700

    KaelaBonk Embed and Probability Update

--- commands/kaelabonk.js
+++ commands/kaelabonk.js
@@ -6,18 +6,22 @@ module.exports = {
     description: "This command will give your forging result",
     async execute(message, args, cmd, client, Discord){
         var embed = new Discord.MessageEmbed()
-        if(message.channel.id == "825593098187833344"||message.channel.id=="966341480186789918"|| message.channel.id=="918356347605643290"){
+        if(message.channel.id == "825593098187833344"||message.channel.id=="966341480186789918"){
             console.log("Testing KaelaBonk")
             const KaelaBonkLevelPlayer = await client.createBonkLevel(message.author);
             console.log(KaelaBonkLevelPlayer)
             var condition = ["succeed","failed"];
-            if (KaelaBonkLevelPlayer.level == 0){
-                var failed_prob = 50
-            } else {
-                var failed_prob = 50 + KaelaBonkLevelPlayer.level
+            if (KaelaBonkLevelPlayer.level <= 25){
+                var succeed_prob = 0.8;
+            } else if (KaelaBonkLevelPlayer.level >= 26 && KaelaBonkLevelPlayer.level <= 50){
+                var succeed_prob = 0.6;
+            } else if (KaelaBonkLevelPlayer.level >= 51 && KaelaBonkLevelPlayer.level <= 75){
+                var succeed_prob = 0.3;
+            } else if (KaelaBonkLevelPlayer.level >= 76){
+                var succeed_prob = 0.1;
             }
             
-            var succeed_prob = 50
+            var failed_prob = 1 - succeed_prob;
             console.log(succeed_prob,failed_prob)
             
             var likelihoods = [succeed_prob,failed_prob];
@@ -26,8 +30,22 @@ module.exports = {
             
             var upgradeBonkCount = upgrade_bonk_count(KaelaBonkLevelPlayer.level)
             var upgrade = (KaelaBonkLevelPlayer.bonk_count >= upgradeBonkCount)
-            var succeedEmbed = {
-                
+            var Embed = {
+                color: `#e67e22`,
+                author: {
+                    name:`${message.author.tag}`,
+                    icon_url:message.author.avatarURL(),
+                },
+                thumbnail: {
+                    url:null
+                },
+                image: {
+                    url: null
+                },
+                footer: {
+                    text: `Success chance ${succeed_prob*100}%`,
+                },
+            
             }
             if(upgrade){
                 // You have reach the amount of bonk for an upgrade!
@@ -35,20 +53,18 @@ module.exports = {
                 if (result =="succeed"){
                     console.log("11")
                     await KaelaBonkLevel.findOneAndUpdate({_id:KaelaBonkLevelPlayer._id},{bonk_count: KaelaBonkLevelPlayer.bonk_count += 1, level:  KaelaBonkLevelPlayer.level += 1})
-                    await message.channel.send({embeds : [embed
-                        .setImage('http://drive.google.com/uc?export=view&id=1dbdS2pWIGWIob1jU84Rf4AMuUC1Si56f')
-                        .setTitle(`${message.author.tag}\nForging succeed!`)
-                        .setDescription("You have reach the amount of successful bonk for an upgrade!\n") 
-                        .addFields(
-                            { name :"Upgrade succeed", value: `<a:kaelayaay:957292452660772914> level ${KaelaBonkLevelPlayer.level-1} ▶️ ${KaelaBonkLevelPlayer.level}`, inline: true},
-                            { name :"Bonk count", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
-                            { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
-                        )
-                        .setThumbnail(message.author.avatarURL())
-                    ]}).catch(err => {
+                    Embed.title = "Forging succeed!";
+                    Embed.description = "You have reach the amount of successful bonk for an upgrade!";
+                    Embed.fields = [
+                        { name :"Upgrade succeed", value: `<a:kaelayaay:957292452660772914> level ${KaelaBonkLevelPlayer.level-1} ▶️ ${KaelaBonkLevelPlayer.level}`, inline: true},
+                        { name :"Bonk count", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
+                        { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`}
+                    ];
+                    Embed.image.url = `http://drive.google.com/uc?export=view&id=1dbdS2pWIGWIob1jU84Rf4AMuUC1Si56f`;
+                    await message.channel.send({embeds : [Embed]}).catch(err => {
                         console.log(err);
                         if (err instanceof Discord.HTTPError) {
-                            setTimeout(() => command.execute(message, args, cmd, client, Discord), 10000)
+                            setTimeout(() => message.channel.send({embeds : [Embed]}), 10000)
                         }
                         return;
                     });
@@ -58,20 +74,18 @@ module.exports = {
                 // Unfortunately, your upgrade failed
                 if (result =="failed"){
                     console.log("10")
-                    await message.channel.send({embeds : [embed
-                        .setImage('http://drive.google.com/uc?export=view&id=1exSrTK0XEEVNzrvL87IgRMTA9Qd0K5EP')
-                        .setTitle(`${message.author.tag}\nForging Failed D:`)
-                        .setDescription("You have reach the amount of bonk for an upgrade!\nUnfortunately, your upgrade failed <:kaelasob:956587823673274389>")
-                        .addFields(
-                            { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
-                            { name :"Bonk count ", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`, inline: true},
-                            { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
-                        )
-                        .setThumbnail(message.author.avatarURL())
-                    ]}).catch(err => {
+                    Embed.title = "Forging Failed D:";
+                    Embed.description = "You have reach the amount of bonk for an upgrade!\nUnfortunately, your upgrade failed <:kaelasob:956587823673274389>";
+                    Embed.fields = [
+                        { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
+                        { name :"Bonk count ", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`, inline: true},
+                        { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
+                    ];
+                    Embed.thumbnail.url = 'http://drive.google.com/uc?export=view&id=1exSrTK0XEEVNzrvL87IgRMTA9Qd0K5EP';
+                    await message.channel.send({embeds : [Embed]}).catch(err => {
                         console.log(err);
                         if (err instanceof Discord.HTTPError) {
-                            setTimeout(() => command.execute(message, args, cmd, client, Discord), 10000)
+                            setTimeout(() => message.channel.send({embeds : [Embed]}), 10000)
                         }
                         return;
                     });
@@ -81,19 +95,17 @@ module.exports = {
                 if (result =="succeed"){
                     console.log("01")
                     await KaelaBonkLevel.findOneAndUpdate({_id:KaelaBonkLevelPlayer._id},{bonk_count: KaelaBonkLevelPlayer.bonk_count +=1})
-                    await message.channel.send({embeds : [embed
-                        .setImage('http://drive.google.com/uc?export=view&id=1dbdS2pWIGWIob1jU84Rf4AMuUC1Si56f')
-                        .setTitle(`${message.author.tag}\nForging succeed!`)
-                        .addFields(
-                            { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
-                            { name :"Bonk count ", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
-                            { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
-                        )
-                        .setThumbnail(message.author.avatarURL())
-                    ]}).catch(err => {
+                    Embed.title = "Forging succeed!";
+                    Embed.fields = [
+                        { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
+                        { name :"Bonk count ", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
+                        { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
+                    ];
+                    Embed.thumbnail.url = 'http://drive.google.com/uc?export=view&id=1dbdS2pWIGWIob1jU84Rf4AMuUC1Si56f';
+                    await message.channel.send({embeds : [Embed]}).catch(err => {
                         console.log(err);
                         if (err instanceof Discord.HTTPError) {
-                            setTimeout(() => command.execute(message, args, cmd, client, Discord), 10000)
+                            setTimeout(() => message.channel.send({embeds : [Embed]}), 10000)
                         }
                         return;
                     });
@@ -101,19 +113,17 @@ module.exports = {
                 }
                 if (result =="failed"){
                     console.log("00")
-                    await message.channel.send({embeds : [embed
-                        .setImage('http://drive.google.com/uc?export=view&id=1exSrTK0XEEVNzrvL87IgRMTA9Qd0K5EP')
-                        .setTitle(`${message.author.tag}\nForging Failed D:`)
-                        .addFields(
-                            { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
-                            { name :"Bonk count", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
-                            { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
-                        )
-                        .setThumbnail(message.author.avatarURL())
-                    ]}).catch(err => {
+                    Embed.title = "Forging Failed D:";
+                    Embed.fields = [
+                        { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
+                        { name :"Bonk count", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
+                        { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
+                    ];
+                    Embed.thumbnail.url = 'http://drive.google.com/uc?export=view&id=1exSrTK0XEEVNzrvL87IgRMTA9Qd0K5EP';
+                    await message.channel.send({embeds : [Embed]}).catch(err => {
                         console.log(err);
                         if (err instanceof Discord.HTTPError) {
-                            setTimeout(() => command.execute(message, args, cmd, client, Discord), 10000)
+                            setTimeout(() => message.channel.send({embeds : [Embed]}), 10000)
                         }
                         return;
                     });
