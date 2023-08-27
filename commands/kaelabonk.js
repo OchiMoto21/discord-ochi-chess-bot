912c5247e0850d328216aa4f9f1df846b88a7c77
OchiMoto21 <nikencarolina@gmail.com>
2023-08-28 04:49:48 +0700

    kaelabonk timer update

--- commands/kaelabonk.js
+++ commands/kaelabonk.js
@@ -7,29 +7,8 @@ module.exports = {
     async execute(message, args, cmd, client, Discord){
         var embed = new Discord.MessageEmbed()
         if(message.channel.id == "825593098187833344"||message.channel.id=="966341480186789918" || message.channel.id=="918356347605643290"){
-            console.log("Testing KaelaBonk")
-            const KaelaBonkLevelPlayer = await client.createBonkLevel(message.author);
-            console.log(KaelaBonkLevelPlayer)
-            var condition = ["succeed","failed"];
-            if (KaelaBonkLevelPlayer.level <= 25){
-                var succeed_prob = 0.8;
-            } else if (KaelaBonkLevelPlayer.level >= 26 && KaelaBonkLevelPlayer.level <= 50){
-                var succeed_prob = 0.6;
-            } else if (KaelaBonkLevelPlayer.level >= 51 && KaelaBonkLevelPlayer.level <= 75){
-                var succeed_prob = 0.3;
-            } else if (KaelaBonkLevelPlayer.level >= 76){
-                var succeed_prob = 0.1;
-            }
             
-            var failed_prob = 1 - succeed_prob;
-            console.log(succeed_prob,failed_prob)
-            
-            var likelihoods = [succeed_prob,failed_prob];
-            var result = condition[simulateEvent(likelihoods)];
-            console.log(result)
-            
-            var upgradeBonkCount = upgrade_bonk_count(KaelaBonkLevelPlayer.level)
-            var upgrade = (KaelaBonkLevelPlayer.bonk_count >= upgradeBonkCount)
+            // Set up the embed message
             var Embed = {
                 color: `#e67e22`,
                 author: {
@@ -43,21 +22,78 @@ module.exports = {
                     url: null
                 },
                 footer: {
-                    text: `Success chance ${succeed_prob*100}%`,
+                    text: null,
                 },
             
             }
+            if (message.author.discriminator == '0') {Embed.author.name = `${message.author.username}`};
+
+            // Start
+            console.log("Testing KaelaBonk")
+            
+            // Take the player's database
+            const KaelaBonkLevelPlayer = await client.createBonkLevel(message.author);
+            console.log(KaelaBonkLevelPlayer)
+
+            // Convert to unix timestamp https://stackoverflow.com/questions/46237707/node-js-converting-date-string-to-unix-timestamp
+            var createdAtUnix = Math.floor(message.createdAt.getTime()/1000);
+            var lastBonkTimeUnix = Math.floor(KaelaBonkLevelPlayer.createdTimestamp.getTime()/1000);
+            // Check the cooldown time
+            var timeDifference = message.createdAt.getTime() - KaelaBonkLevelPlayer.createdTimestamp.getTime();
+            
+            if ( timeDifference < 60*30*1000) {
+                var NextBonkTime = lastBonkTimeUnix + 60*30;    
+                Embed.author = null;
+                Embed.description = `**${message.author.username}** next bonk is <t:${NextBonkTime}:R> <a:elanguinsway:967462309276573727>`;
+                await message.channel.send({
+                    embeds : [Embed]
+                    })
+                return;
+            }
+            
+            // Determine the success probability
+            if (KaelaBonkLevelPlayer.level <= 25){
+                var succeed_prob = 0.8;
+            } else if (KaelaBonkLevelPlayer.level >= 26 && KaelaBonkLevelPlayer.level <= 50){
+                var succeed_prob = 0.6;
+            } else if (KaelaBonkLevelPlayer.level >= 51 && KaelaBonkLevelPlayer.level <= 75){
+                var succeed_prob = 0.3;
+            } else if (KaelaBonkLevelPlayer.level >= 76 && KaelaBonkLevelPlayer.level <= 100){
+                var succeed_prob = 0.1;
+            } else if (KaelaBonkLevelPlayer.level >= 101){
+                var succeed_prob = 0.05;
+            }
+
+            // Determine the fail probability
+            var failed_prob = 1 - succeed_prob;
+            console.log(succeed_prob,failed_prob)
+            
+            // Determine success and fail
+            var condition = ["succeed","failed"];
+            var likelihoods = [succeed_prob,failed_prob];
+            var result = condition[simulateEvent(likelihoods)];
+            console.log(result)
+            
+            // Determine if the player got an upgrade or not
+            var upgradeBonkCount = upgrade_bonk_count(KaelaBonkLevelPlayer.level);
+            var upgrade = (KaelaBonkLevelPlayer.bonk_count >= upgradeBonkCount);
+            Embed.footer = {
+                text:`Success chance ${succeed_prob*100}%`
+            }
+
+            
             if(upgrade){
                 // You have reach the amount of bonk for an upgrade!
                 // An upgrade have been successfully made <a:kaelayaay:957292452660772914>
                 if (result =="succeed"){
                     console.log("11")
-                    await KaelaBonkLevel.findOneAndUpdate({_id:KaelaBonkLevelPlayer._id},{bonk_count: KaelaBonkLevelPlayer.bonk_count += 1, level:  KaelaBonkLevelPlayer.level += 1})
+                    await KaelaBonkLevel.findOneAndUpdate({_id:KaelaBonkLevelPlayer._id},{bonk_count: KaelaBonkLevelPlayer.bonk_count += 1, level:  KaelaBonkLevelPlayer.level += 1, createdTimestamp: message.createdAt})
                     Embed.title = "Forging succeed!";
                     Embed.description = "You have reach the amount of successful bonk for an upgrade!";
                     Embed.fields = [
                         { name :"Upgrade succeed", value: `<a:kaelayaay:957292452660772914> level ${KaelaBonkLevelPlayer.level-1} ▶️ ${KaelaBonkLevelPlayer.level}`, inline: true},
                         { name :"Bonk count", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
+                        { name :"Next bonk", value: `<t:${createdAtUnix+60*30}:R>`,inline: true},
                         { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`}
                     ];
                     Embed.image.url = `https://cdn.discordapp.com/attachments/1006146433247891566/1061309084982325318/smithing_success.png`;
@@ -74,11 +110,13 @@ module.exports = {
                 // Unfortunately, your upgrade failed
                 if (result =="failed"){
                     console.log("10")
+                    await KaelaBonkLevel.findOneAndUpdate({_id:KaelaBonkLevelPlayer._id},{createdTimestamp: message.createdAt})
                     Embed.title = "Forging Failed D:";
                     Embed.description = "You have reach the amount of bonk for an upgrade!\nUnfortunately, your upgrade failed <:kaelasob:956587823673274389>";
                     Embed.fields = [
                         { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
                         { name :"Bonk count ", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`, inline: true},
+                        { name :"Next bonk", value: `<t:${createdAtUnix+60*30}:R>`,inline: true},
                         { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
                     ];
                     Embed.thumbnail.url = 'https://cdn.discordapp.com/attachments/1006146433247891566/1061309084458029256/smithing_fail.png';
@@ -94,11 +132,12 @@ module.exports = {
             } else {
                 if (result =="succeed"){
                     console.log("01")
-                    await KaelaBonkLevel.findOneAndUpdate({_id:KaelaBonkLevelPlayer._id},{bonk_count: KaelaBonkLevelPlayer.bonk_count +=1})
+                    await KaelaBonkLevel.findOneAndUpdate({_id:KaelaBonkLevelPlayer._id},{bonk_count: KaelaBonkLevelPlayer.bonk_count +=1,createdTimestamp: message.createdAt})
                     Embed.title = "Forging succeed!";
                     Embed.fields = [
                         { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
                         { name :"Bonk count ", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
+                        { name :"Next bonk", value: `<t:${createdAtUnix+60*30}:R>`,inline: true},
                         { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
                     ];
                     Embed.thumbnail.url = 'https://cdn.discordapp.com/attachments/1006146433247891566/1061309084982325318/smithing_success.png';
@@ -113,10 +152,12 @@ module.exports = {
                 }
                 if (result =="failed"){
                     console.log("00")
+                    await KaelaBonkLevel.findOneAndUpdate({_id:KaelaBonkLevelPlayer._id},{createdTimestamp: message.createdAt})
                     Embed.title = "Forging Failed D:";
                     Embed.fields = [
                         { name :"Your level", value: `<a:elanguinsway:967462309276573727> level ${KaelaBonkLevelPlayer.level}`,inline: true},
                         { name :"Bonk count", value: `<:kaelanoted:961838924379062302> ${KaelaBonkLevelPlayer.bonk_count} bonk(s)`,inline: true},
+                        { name :"Next bonk", value: `<t:${createdAtUnix+60*30}:R>`,inline: true},
                         { name :"Bonk needed for upgrade", value: `<a:kaelanumahammer:962582163251949578> ${bonkRequired(KaelaBonkLevelPlayer.level,KaelaBonkLevelPlayer.bonk_count)} bonk(s)`},
                     ];
                     Embed.thumbnail.url = 'https://cdn.discordapp.com/attachments/1006146433247891566/1061309084458029256/smithing_fail.png';
--- schemas/KaelaBonkLevel.js
+++ schemas/KaelaBonkLevel.js
@@ -3,7 +3,8 @@ const KaelaBonkLevelSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     memberId: String,
     bonk_count: {type : Number, default: 0},
-    level: {type : Number, default: 1}
+    level: {type : Number, default: 1},
+    createdTimestamp: {type : Date, default : null}
 });
 
 module.exports = mongoose.model('KaelaBonkLevel', KaelaBonkLevelSchema,'KaelaBonkLevels');
